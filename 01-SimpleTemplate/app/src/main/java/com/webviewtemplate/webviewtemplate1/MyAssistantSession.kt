de797f57c95886ec5a8b0f53c02f04c23d99be98
Mulher amparada <mulheramparada@gmail.com>
2026-07-13 11:41:45 -0300

    Criar MyAssistantSession.kt

---
+++ 01-SimpleTemplate/app/src/main/java/com/webviewtemplate/webviewtemplate1/MyAssistantSession.kt
@@ -0,0 +1,10 @@
+package com.webviewtemplate.webviewtemplate1
+
+import android.content.Context
+import android.service.voice.VoiceInteractionSession
+
+class MyAssistantSession(
+    context: Context
+) : VoiceInteractionSession(context) {
+
+}
