61b7a5f4ea78b47adfc249fffb7226588990d113
Mulher amparada <mulheramparada@gmail.com>
2026-07-13 12:38:54 -0300

    Atualizar o MainActivity.kt

--- 01-SimpleTemplate/app/src/main/java/com/webviewtemplate/webviewtemplate1/MainActivity.kt
+++ 01-SimpleTemplate/app/src/main/java/com/webviewtemplate/webviewtemplate1/MainActivity.kt
@@ -1,42 +1,47 @@
 package com.webviewtemplate.webviewtemplate1
 
-
 import android.Manifest
+import android.app.Dialog
 import android.app.admin.DevicePolicyManager
 import android.content.ComponentName
-import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
+import android.os.Bundle
+import android.speech.RecognitionListener
 import android.speech.RecognizerIntent
 import android.speech.SpeechRecognizer
-import android.widget.*
-import android.graphics.Color
-import android.graphics.drawable.GradientDrawable
-import java.util.Locale
-import androidx.core.app.ActivityCompat
-import androidx.core.content.ContextCompat
-import android.os.Bundle
 import android.view.View
 import android.view.WindowManager
+import android.widget.Button
 import android.widget.FrameLayout
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.core.view.WindowCompat
 import androidx.core.view.WindowInsetsControllerCompat
 
 class MainActivity : AppCompatActivity() {
 
-private val REQUEST_PERMISSIONS = 100
 
-private lateinit var adminComponent: ComponentName
-private lateinit var speechRecognizer: SpeechRecognizer
+    private val REQUEST_PERMISSIONS = 100
+
+    private lateinit var adminComponent: ComponentName
+
+    private lateinit var speechRecognizer: SpeechRecognizer
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
 
         WindowCompat.setDecorFitsSystemWindows(
             window,
             false
         )
+
 
         window.apply {
 
@@ -47,20 +52,24 @@ private lateinit var speechRecognizer: SpeechRecognizer
             statusBarColor = Color.TRANSPARENT
             navigationBarColor = Color.TRANSPARENT
 
+
             if (android.os.Build.VERSION.SDK_INT >= 28) {
                 navigationBarDividerColor = Color.TRANSPARENT
             }
+
 
             if (android.os.Build.VERSION.SDK_INT >= 29) {
                 isStatusBarContrastEnforced = false
                 isNavigationBarContrastEnforced = false
             }
 
+
             decorView.systemUiVisibility =
                 View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
-                View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or
-                View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or
+                        View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
         }
+
 
         WindowInsetsControllerCompat(
             window,
@@ -77,210 +86,249 @@ private lateinit var speechRecognizer: SpeechRecognizer
 
         setContentView(root)
 
+
         pedirPermissoes()
-        
+
         abrirPopupVoz()
+
     }
+
 
     private fun pedirPermissoes() {
 
-    val permissoes = arrayOf(
-        Manifest.permission.RECORD_AUDIO,
-        Manifest.permission.READ_CONTACTS,
-        Manifest.permission.ACCESS_FINE_LOCATION,
-        Manifest.permission.ACCESS_COARSE_LOCATION,
-        Manifest.permission.READ_SMS,
-        Manifest.permission.SEND_SMS,
-        Manifest.permission.READ_PHONE_STATE,
-        Manifest.permission.CALL_PHONE
-    )
+        val permissoes = arrayOf(
 
+            Manifest.permission.RECORD_AUDIO,
+            Manifest.permission.READ_CONTACTS,
+            Manifest.permission.ACCESS_FINE_LOCATION,
+            Manifest.permission.ACCESS_COARSE_LOCATION,
+            Manifest.permission.READ_SMS,
+            Manifest.permission.SEND_SMS,
+            Manifest.permission.READ_PHONE_STATE,
+            Manifest.permission.CALL_PHONE
 
-    val faltando = permissoes.filter {
-
-        ContextCompat.checkSelfPermission(
-            this,
-            it
-        ) != PackageManager.PERMISSION_GRANTED
-
-    }
-
-
-    if (faltando.isNotEmpty()) {
-
-        ActivityCompat.requestPermissions(
-            this,
-            faltando.toTypedArray(),
-            REQUEST_PERMISSIONS
         )
 
-    } else {
 
-        ativarAdministrador()
+        val faltando = permissoes.filter {
 
-    }
-}
+            ContextCompat.checkSelfPermission(
+                this,
+                it
+            ) != PackageManager.PERMISSION_GRANTED
 
-private fun verificarPermissoes(): Boolean {
-
-    val permissoes = arrayOf(
-        Manifest.permission.RECORD_AUDIO,
-        Manifest.permission.READ_CONTACTS,
-        Manifest.permission.ACCESS_FINE_LOCATION,
-        Manifest.permission.READ_SMS,
-        Manifest.permission.READ_PHONE_STATE
-    )
+        }
 
 
-    return permissoes.all {
+        if (faltando.isNotEmpty()) {
 
-        ContextCompat.checkSelfPermission(
-            this,
-            it
-        ) == PackageManager.PERMISSION_GRANTED
+            ActivityCompat.requestPermissions(
+                this,
+                faltando.toTypedArray(),
+                REQUEST_PERMISSIONS
+            )
 
-    }
-}
-
-override fun onRequestPermissionsResult(
-    requestCode: Int,
-    permissions: Array<out String>,
-    grantResults: IntArray
-) {
-    super.onRequestPermissionsResult(
-        requestCode,
-        permissions,
-        grantResults
-    )
-
-
-    if (requestCode == REQUEST_PERMISSIONS) {
-
-        if (grantResults.all {
-                it == PackageManager.PERMISSION_GRANTED
-            }) {
+        } else {
 
             ativarAdministrador()
 
         }
-
     }
-}
-
-private fun ativarAdministrador() {
-
-    adminComponent = ComponentName(
-        this,
-        MeuAdministrador::class.java
-    )
-
-    val intent = Intent(
-        DevicePolicyManager.ACTION_ADD_DEVICE_ADMIN
-    )
-
-    intent.putExtra(
-        DevicePolicyManager.EXTRA_DEVICE_ADMIN,
-        adminComponent
-    )
-
-    intent.putExtra(
-        DevicePolicyManager.EXTRA_ADD_EXPLANATION,
-        "Ative o administrador para liberar funções de segurança"
-    )
-
-    startActivity(intent)
-}
-
-private fun iniciarReconhecimento(texto: TextView) {
-
-    speechRecognizer =
-        SpeechRecognizer.createSpeechRecognizer(this)
 
 
-    val intent = Intent(
-        RecognizerIntent.ACTION_RECOGNIZE_SPEECH
-    )
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+
+        super.onRequestPermissionsResult(
+            requestCode,
+            permissions,
+            grantResults
+        )
 
 
-    intent.putExtra(
-        RecognizerIntent.EXTRA_LANGUAGE_MODEL,
-        RecognizerIntent.LANGUAGE_MODEL_FREE_FORM
-    )
+        if (requestCode == REQUEST_PERMISSIONS) {
 
+            if (grantResults.all {
+                    it == PackageManager.PERMISSION_GRANTED
+                }) {
 
-    intent.putExtra(
-        RecognizerIntent.EXTRA_LANGUAGE,
-        "pt-BR"
-    )
-
-
-    speechRecognizer.setRecognitionListener(
-        object : RecognitionListener {
-
-            override fun onResults(results: Bundle?) {
-
-                val fala =
-                    results?.getStringArrayList(
-                        SpeechRecognizer.RESULTS_RECOGNITION
-                    )?.firstOrNull()
-
-
-                texto.text = fala ?: "Não entendi"
+                ativarAdministrador()
 
             }
-
-
-            override fun onError(error: Int) {
-                texto.text = "Erro de voz"
-            }
-
-            override fun onReadyForSpeech(params: Bundle?) {}
-            override fun onBeginningOfSpeech() {}
-            override fun onRmsChanged(rmsdB: Float) {}
-            override fun onBufferReceived(buffer: ByteArray?) {}
-            override fun onEndOfSpeech() {}
-            override fun onPartialResults(partialResults: Bundle?) {}
-            override fun onEvent(eventType: Int, params: Bundle?) {}
 
         }
-    )
+    }
 
 
-    speechRecognizer.startListening(intent)
-}
 
-private fun abrirPopupVoz() {
+    private fun ativarAdministrador() {
 
-    val dialog = Dialog(this)
-
-    val view = layoutInflater.inflate(
-        R.layout.popup_voz,
-        null
-    )
-
-    val texto = view.findViewById<TextView>(
-        R.id.textoVoz
-    )
-
-    val botao = view.findViewById<Button>(
-        R.id.botaoOuvir
-    )
+        adminComponent = ComponentName(
+            this,
+            MeuAdministrador::class.java
+        )
 
 
-    dialog.setContentView(view)
-
-    dialog.window?.setBackgroundDrawable(
-        ColorDrawable(Color.TRANSPARENT)
-    )
+        val intent = Intent(
+            DevicePolicyManager.ACTION_ADD_DEVICE_ADMIN
+        )
 
 
-    dialog.show()
+        intent.putExtra(
+            DevicePolicyManager.EXTRA_DEVICE_ADMIN,
+            adminComponent
+        )
 
 
-    botao.setOnClickListener {
+        intent.putExtra(
+            DevicePolicyManager.EXTRA_ADD_EXPLANATION,
+            "Ative o administrador para liberar funções de segurança"
+        )
 
-        iniciarReconhecimento(texto)
+
+        startActivity(intent)
 
     }
-}
+
+
+
+    private fun abrirPopupVoz() {
+
+
+        val dialog = Dialog(this)
+
+
+        val view = layoutInflater.inflate(
+            R.layout.popup_voz,
+            null
+        )
+
+
+        val texto = view.findViewById<TextView>(
+            R.id.textoVoz
+        )
+
+
+        val botao = view.findViewById<Button>(
+            R.id.botaoOuvir
+        )
+
+
+        dialog.setContentView(view)
+
+
+        dialog.window?.setBackgroundDrawable(
+            ColorDrawable(Color.TRANSPARENT)
+        )
+
+
+        dialog.show()
+
+
+
+        botao.setOnClickListener {
+
+            iniciarReconhecimento(texto)
+
+        }
+
+    }
+
+
+
+    private fun iniciarReconhecimento(
+        texto: TextView
+    ) {
+
+
+        speechRecognizer =
+            SpeechRecognizer.createSpeechRecognizer(this)
+
+
+
+        val intent = Intent(
+            RecognizerIntent.ACTION_RECOGNIZE_SPEECH
+        )
+
+
+        intent.putExtra(
+            RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+            RecognizerIntent.LANGUAGE_MODEL_FREE_FORM
+        )
+
+
+        intent.putExtra(
+            RecognizerIntent.EXTRA_LANGUAGE,
+            "pt-BR"
+        )
+
+
+
+        speechRecognizer.setRecognitionListener(
+
+            object : RecognitionListener {
+
+
+                override fun onResults(results: Bundle?) {
+
+
+                    val resultado =
+                        results?.getStringArrayList(
+                            SpeechRecognizer.RESULTS_RECOGNITION
+                        )
+
+
+                    if (!resultado.isNullOrEmpty()) {
+
+                        texto.text = resultado[0]
+
+                        Comandos.executar(
+    this,
+    resultado[0]
+)
+
+                    }
+
+                }
+
+
+
+                override fun onError(error: Int) {
+
+                    texto.text =
+                        "Não foi possível reconhecer"
+
+                }
+
+
+
+                override fun onReadyForSpeech(params: Bundle?) {}
+                override fun onBeginningOfSpeech() {}
+                override fun onRmsChanged(rmsdB: Float) {}
+                override fun onBufferReceived(buffer: ByteArray?) {}
+                override fun onEndOfSpeech() {}
+                override fun onPartialResults(partialResults: Bundle?) {}
+
+                override fun onEvent(
+                    eventType: Int,
+                    params: Bundle?
+                ) {}
+
+            }
+
+        )
+
+
+        speechRecognizer.startListening(intent)
+
+    }
+
+
+
+    
+
+
 }
