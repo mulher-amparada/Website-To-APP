33a64ea996094af6a38a8b1575593656a97be788
Mulher amparada <mulheramparada@gmail.com>
2026-07-13 11:40:56 -0300

    Criar MyAssistantService.kt

---
+++ 01-SimpleTemplate/app/src/main/java/com/webviewtemplate/webviewtemplate1/MyAssistantService.kt
@@ -0,0 +1,10 @@
+package com.webviewtemplate.webviewtemplate1
+
+import android.service.voice.VoiceInteractionService
+
+class MyAssistantService : VoiceInteractionService() {
+
+    override fun onReady() {
+        super.onReady()
+    }
+}
