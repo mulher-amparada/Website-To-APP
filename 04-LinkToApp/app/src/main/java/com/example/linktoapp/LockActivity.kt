845cdea5676aec46d7379e9ca9ee6692dab59a9a
Mulher amparada <mulheramparada@gmail.com>
2026-07-18 20:26:40 -0300

    Criar LockActivity.kt

---
+++ 04-LinkToApp/app/src/main/java/com/example/linktoapp/LockActivity.kt
@@ -0,0 +1,80 @@
+package com.linktoapp.app
+
+import android.graphics.Color
+import android.os.Build
+import android.os.Bundle
+import android.view.View
+import android.view.WindowManager
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsControllerCompat
+
+class LockActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+
+        window.apply {
+
+            addFlags(
+                WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS
+            )
+
+            statusBarColor = Color.TRANSPARENT
+            navigationBarColor = Color.TRANSPARENT
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+                navigationBarDividerColor = Color.TRANSPARENT
+            }
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+                isStatusBarContrastEnforced = false
+                isNavigationBarContrastEnforced = false
+            }
+
+            decorView.systemUiVisibility =
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
+                View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or
+                View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+        }
+
+        WindowInsetsControllerCompat(
+            window,
+            window.decorView
+        ).apply {
+            isAppearanceLightStatusBars = false
+            isAppearanceLightNavigationBars = false
+        }
+
+        setContentView(R.layout.activity_lock)
+
+        val pacote = intent.getStringExtra("package") ?: run {
+            finish()
+            return
+        }
+
+        BiometricHelper(this).autenticar(
+
+            sucesso = {
+
+                UnlockManager.desbloquear(pacote)
+
+                finish()
+
+            },
+
+            erro = {
+
+                finishAffinity()
+
+            }
+
+        )
+    }
+
+    override fun onBackPressed() {
+        // impede sair usando voltar
+    }
+}
