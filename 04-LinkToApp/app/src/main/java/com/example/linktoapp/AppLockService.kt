395df905b3b183f2e01560014b98fd4743dafc42
Mulher amparada <mulheramparada@gmail.com>
2026-07-21 13:15:50 -0300

    Atualizar o AppLockService.kt

--- 04-LinkToApp/app/src/main/java/com/example/linktoapp/AppLockService.kt
+++ 04-LinkToApp/app/src/main/java/com/example/linktoapp/AppLockService.kt
@@ -17,39 +17,70 @@ class AppLockService : AccessibilityService() {
 
     override fun onAccessibilityEvent(event: AccessibilityEvent?) {
 
-        if (event == null) return
+        if (event == null)
+            return
 
         if (event.eventType != AccessibilityEvent.TYPE_WINDOW_STATE_CHANGED)
             return
 
         val pacote = event.packageName?.toString() ?: return
 
-if (
-    pacote == "com.android.systemui" ||
-    pacote == "com.google.android.inputmethod.latin"
-)
-    return
+
+        if (LockState.voltarPressionado) {
+
+            LockState.voltarPressionado = false
+
+            val intent = Intent(
+                this,
+                LockActivity::class.java
+            )
+
+            intent.putExtra(
+                "package",
+                pacote
+            )
+
+            intent.addFlags(
+                Intent.FLAG_ACTIVITY_NEW_TASK or
+                Intent.FLAG_ACTIVITY_CLEAR_TOP or
+                Intent.FLAG_ACTIVITY_EXCLUDE_FROM_RECENTS
+            )
+
+            startActivity(intent)
+
+            return
+        }
+
+
+        if (
+            pacote == "com.android.systemui" ||
+            pacote == "com.google.android.inputmethod.latin"
+        )
+            return
+
 
         // Ignora o próprio AppLock
-        if (pacote == packageName) {
+        if (pacote == packageName)
             return
-        }
 
-        // Mesmo aplicativo: ignora troca de Activity
-        if (pacote == ultimoPacote) {
+
+        // Ignora mudança de Activity dentro do mesmo app
+        if (pacote == ultimoPacote)
             return
-        }
 
-        // Novo aplicativo aberto
+
         ultimoPacote = pacote
 
-        // Verifica se está protegido
+
+        // Verifica se o app está protegido
         if (!repository.protegido(pacote))
             return
 
-        // Evita abrir várias telas de bloqueio
+
+        // Evita abrir várias LockActivity
         if (LockActivity.aberta)
             return
+
 
         val intent = Intent(
             this,
@@ -68,6 +99,7 @@ if (
         )
 
         startActivity(intent)
+
     }
 
     override fun onInterrupt() {
