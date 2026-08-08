ec8435bf71ce3bc1fb6054a3c341c6f685760209
Mulher amparada <mulheramparada@gmail.com>
2026-08-08 11:18:47 -0300

    Atualizar o WebAppInterface.kt

--- 02-OfflineAppTemplate/app/src/main/java/com/mulheres/WebAppInterface.kt
+++ 02-OfflineAppTemplate/app/src/main/java/com/mulheres/WebAppInterface.kt
@@ -1,6 +1,11 @@
 package com.mulheres
 
 import org.json.JSONArray
+import android.Manifest
+import android.os.Build
+import android.content.pm.PackageManager
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import org.json.JSONObject
 import android.graphics.Bitmap
 import android.graphics.Canvas
